9898f8b71b992bc0d0da88688ff804c9f9ab2e6e
Elise <lockwoelis@myvuw.ac.nz>
2024-08-01 11:55:53 +1200

    Update my wallpaper final
    Handed in via nuku and 'commit to main' on here to - thx

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -11,7 +11,7 @@ let size = 5 // parameterized control over size of buttons
 
 function setup_wallpaper(pWallpaper) {
 pWallpaper.output_mode(GLIDE_WALLPAPER);
-pWallpaper.resolution(NINE_LANDSCAPE);
+pWallpaper.resolution(A3);
 pWallpaper.show_guide(false); //set this to false when you're ready to print
 
 //Grid settings
